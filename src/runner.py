0b7ff5f6335e229be3cace6ff347b7277ef04a10
Niloufar Beyranvand <nbeyran@tiger.eecs.yorku.ca>
2024-01-04 22:10:58 -0500

    Implemented class weight handling, GPU selection, and early stopping for model training

--- src/runner.py
+++ src/runner.py
@@ -1,13 +1,18 @@
+import os
 import sys
-sys.path.append('/home/nbeyran/Hate_Speech_Detection')
-sys.path.append('/home/nbeyran/Hate_Speech_Detection/src')
+sys.path.append('..')
+sys.path.append('../..')
+
 
 from lightning import Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+
 from lightning.pytorch import loggers as pl_loggers
 
 from pytorch_lightning import seed_everything
 from sklearn.model_selection import train_test_split
+import torch
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 
@@ -46,6 +51,8 @@ def main(args):
     - freeze_lm: Flag to indicate whether to freeze the language model during training.
     - number_of_classes: Number of classes for the classification task.
     """
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu # specify which GPU(s) to be used
+
     tweets, lables = load_and_process(path=f"{BASE_PATH}/{args.data_path}")
 
     seed_everything(args.random_state)     # Set seed for reproducibility
@@ -77,24 +84,41 @@ def main(args):
                                         batch_size=args.validation_batch_size,
                                         shuffle=True)
     
+    if args.class_weights:
+      check_point_path = BASE_PATH / "checkpoints"/ f"{args.model_checkpoint_path}/{args.language_model_name_or_path}/class_weights/"
+    else:
+      check_point_path = BASE_PATH / "checkpoints" / f"{args.model_checkpoint_path}/{args.language_model_name_or_path}/no_class_weights/"
     model_checkpoint = ModelCheckpoint(
-        dirpath= BASE_PATH / f"{args.model_checkpoint_path}",
-        filename='{args.language_model_name_or_path}-{epoch:02d}-{val_loss:.2f}',
+        dirpath= check_point_path,
+        filename='{epoch:02d}-{val_loss:.2f}',
         save_top_k=1,
         verbose=True,
         monitor='val_loss',
         mode='min'
     )
 
+    if args.class_weights:
+       # Calculate the number of instances of each class
+      class_counts = torch.bincount(torch.tensor(train_labels))
+
+      # Calculate class weights inversely proportional to their frequency
+      class_weights = 1. / class_counts.float()
+    
+      device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+      class_weights.to(device)
     model = HateDetection_LM_CNN(language_model_name_or_path= args.language_model_name_or_path,
                                   max_length=args.max_length,
                                   freeze_lm=args.freeze_lm,
-                                  number_of_classes=args.number_of_classes)
-    
-    tb_logger = pl_loggers.TensorBoardLogger({BASE_PATH}/ f"logs/{args.language_model_name_or_path}")
+                                  number_of_classes=args.number_of_classes, 
+                                  class_weights=class_weights if args.class_weights else None)
+    if args.class_weights:
+      logger_path = f"{BASE_PATH}/logs/{args.language_model_name_or_path}/class_weights/"
+    else:
+      logger_path = f"{BASE_PATH}/logs/{args.language_model_name_or_path}"
+    tb_logger = pl_loggers.TensorBoardLogger(logger_path)
     
     if args.do_train:
-      trainer = Trainer(max_epochs=args.max_epochs, callbacks=[model_checkpoint], logger=tb_logger)
+      trainer = Trainer(max_epochs=args.max_epochs, callbacks=[model_checkpoint, EarlyStopping(monitor="val_loss", mode="min")], logger=tb_logger)
       trainer.fit(model, train_data_loader, validation_data_loader)
     
     if args.do_test:
@@ -107,8 +131,14 @@ def main(args):
       test_data_loader = DataLoader(list(zip(test_input_ids, test_attention_masks, test_labels)),
                                         batch_size=args.validation_batch_size,
                                         shuffle=True)
-    
-      model = HateDetection_LM_CNN.load_from_checkpoint(args.model_checkpoint_path)
+      
+      checkpoint_name = os.listdir(check_point_path)[0]
+      checkpoint_path = os.path.join(check_point_path, checkpoint_name)
+      model = HateDetection_LM_CNN.load_from_checkpoint(checkpoint_path=checkpoint_path,
+                                                        language_model_name_or_path=args.language_model_name_or_path,
+                                                        class_weights = class_weights if args.class_weights else None
+                                                      )
+      trainer = Trainer(logger=tb_logger)      
       trainer.test(model, test_data_loader)
 
 
